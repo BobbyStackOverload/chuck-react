e0bd9623701cb8300e85fce1a1d09413f1861bd1
Bobby W Bryant <sirbob1999@gmail.com>
2020-03-24 20:46:14 -0400

    prop work

---
+++ src/components/refreshQuote.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class RefreshQuote extends Component {
+  render() {
+    const { RefreshQuote } =this.props;
+    return (
+        <p>{RefreshQuote}</p>
+        
+    );
+  }
+}
+
+export default RefreshQuote;
